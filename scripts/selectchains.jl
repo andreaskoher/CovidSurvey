e10b8afbb0d9d42cfdd036c7033583a1d7a2fb6d
andreaskoher <ankoh@dtu.dk>
2021-06-23 10:08:08 +0200

    update analysis for meeting on June 23 on the parametric model with the new data set

--- scripts/selectchains.jl
+++ scripts/selectchains.jl
@@ -13,12 +13,15 @@ using Plots.PlotMeasures
 using Random
 using BSON
 using PrettyTables
+using Turing
+plotlyjs()
 ## ===========================================================================
 # load data and model
 # ============================================================================
-fdir = projectdir("reports/parametric-model-on-2nd-wave_#2/")
-fname = "cases=2020-06-01_chains=10_hospit=true_model=parametric-cases_observ=2021-01-13_preds=CF,CC,CR,CS_sero=true_steps=4000_warmup=1000"
-PREFIX = ""
+fdir = projectdir("reports/parametric-model-on-2nd-wave-newdata/")
+# fname = "cases=2020-06-01_chains=10_hospit=true_model=parametric-cases_observ=2021-01-13_preds=CF,CC,CR,CS_sero=true_steps=2000_warmup=1000"
+fname = "cases=2020-06-01_chains=10_hospit=true_model=parametric-cases_observ=2021-01-13_preds=CF,CC,CR,CS_sero=true_steps=2000_warmup=1000"
+PREFIX = "RAW-"
 epimodel = CovidSurvey.National.model_parametric_cases
 #-----------------------------------------------------------------------------
 # read params
@@ -36,18 +39,26 @@ data = National.load_data(
     , ps["sero"]
     , iar_step = 1
     , covariates_kwargs = Dict(
-        :fname => normpath( homedir(), "data/covidsurvey/smoothed_contacts.csv" ),
+        :fname => projectdir("data", "inferred_contact_rates.csv"),
+        #:fname => projectdir("data", "smoothed_contact_rates.csv"), #normpath( homedir(), "data/covidsurvey/smoothed_contacts.csv" ),
+        # :fname => normpath( homedir(), "data/covidsurvey/smoothed_contacts.csv" ),
         :shift => -1,
-        :startdate => "2020-11-10", # >= 1000 cases / day
-        # :enddate => "2021-01-13" # <= 1000 cases / day
+        :startdate => "2020-11-10",
+        # :enddate => "2021-01-13"
     )
 )
 turing_data = data.turing_data;
 
 #-----------------------------------------------------------------------------
 # read chain
+function not(c::Chains, i)
+    n = size(c, 3)
+    s = filter(x->x∉i, 1:n)
+    return c[:,:,s]
+end
 chain = read(fdir*PREFIX*"CHAIN_"*fname*".jls", Chains)
-chain = chain[1:10:end,:,[2,4,5,6,7,8,9]]
+chain = chain[1:10:end,:,:]
+chain = not(chain, [2,4])
 # fname = normpath( fdir, savename(PREFIX*"CHAIN", ps, "jls") )
 # safesave( fname, chain)
 
@@ -56,15 +67,19 @@ chain = chain[1:10:end,:,[2,4,5,6,7,8,9]]
 n = filter( x->!occursin(r"(?<!α)\[", x), String.(names(chain)))
 let
     p = plot(chain[n])
-    fname = normpath( fdir, savename(PREFIX*"CHAINSPLOT", ps, "html") )
-    savefig(p, fname )
-    run(`firefox $(fname)`, wait=false)
+    saveto = normpath( fdir, PREFIX*"CHAINSPLOT_"*fname*".html")
+    savefig(p, saveto )
+    run(`firefox $(saveto)`, wait=false)
 end
 
 ## ===========================================================================
 # plot results
 # ============================================================================
 # make predictions
+m_pred = epimodel(turing_data..., true; link=KLogistic(3.), invlink=KLogit(3.))
+gq = Turing.generated_quantities(m_pred, chain)
+generated_posterior = vectup2tupvec(reshape(gq, length(gq)))
+
 posterior = let
     m_pred = epimodel(turing_data..., true; link=KLogistic(3.), invlink=KLogit(3.) )
     gq = Turing.generated_quantities(m_pred, chain)
@@ -76,8 +91,8 @@ end;
 @info "store generated quantities"
 let
     dic = Dict( zip( keys(posterior), values(posterior) ) )
-    fname = normpath( fdir, savename(PREFIX*"GENERATED-QUANTITIES", ps, "bson") )
-    bson( fname ,  dic )
+    saveto = normpath( fdir, PREFIX*"GENERATED-QUANTITIES_"*fname*".bson")
+    bson( saveto ,  dic )
 end
 #-----------------------------------------------------------------------------
 @info "store reproduction number"
@@ -93,33 +108,33 @@ rt = let
         date = data.dates[ib:ie]
         DataFrame((;date, llq, lq, mq, uq, uuq))
     end
-    fname = normpath( fdir, savename(PREFIX*"Rt", ps, "csv") )
-    save(fname, rt)
+    saveto = normpath( fdir, PREFIX*"Rt_"*fname*".csv")
+    save(saveto, rt)
     rt
 end
 #-----------------------------------------------------------------------------
 @info "plot results"
 let
     p = National.plot_results(data, posterior...);
-    fname = normpath( fdir, savename(PREFIX*"PREDICTION", ps, "html") )
-    savefig(p, fname )
+    saveto = normpath( fdir, PREFIX*"PREDICTION_"*fname*".html")
+    savefig(p, saveto )
     # savefig(p, projectdir("figures", fname*".png") )
-    run(`firefox $(fname)`, wait=false)
+    run(`firefox $(saveto)`, wait=false)
 end
 #-----------------------------------------------------------------------------
 # perform diagnostics
 if ps["chains"] > 1
     @info "gelman diagnostics"
     diagnostics = gelmandiag(chain)
-    fname = normpath( fdir, savename(PREFIX*"GELMANDIAG", ps, "csv") )
-    safesave( fname, diagnostics)
+    saveto = normpath( fdir, PREFIX*"GELMANDIAG_"*fname*".csv")
+    safesave( saveto, diagnostics)
     pretty_table(diagnostics; crop=:none)
 end
 #-----------------------------------------------------------------------------
 @info "meanplot"
 let
     p = meanplot(chain[n]);
-    fname = normpath( fdir, savename(PREFIX*"MEANPLOT", ps, "html") )
-    savefig(p, fname )
-    run(`firefox $(fname)`, wait=false)
+    saveto = normpath( fdir, PREFIX*"MEANPLOT_"*fname*".html")
+    savefig(p, saveto )
+    run(`firefox $(saveto)`, wait=false)
 end
