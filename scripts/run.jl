63bb8c1769b25858294e0df8a57739fc9593ad1d
andreaskoher <ankoh@dtu.dk>
2021-08-31 16:44:30 +0200

    clean up and adapt use new processing method

--- scripts/run.jl
+++ scripts/run.jl
@@ -2,26 +2,9 @@ using DrWatson
 quickactivate(@__DIR__)
 @show projectdir()
 #-----------------------------------------------------------------------------
-# load packages
-@info "load packages"
+# load parameters
 using ArgParse
-using CovidSurvey
-using Random, Dates, Turing
-using Base.Threads
-using StatsPlots
-using PrettyTables
-using DataFrames
-using BSON
-
-using Memoization
-using ReverseDiff
-Turing.setrdcache(true)
-setadbackend(:reversediff)
-
-plotlyjs()
-@info "number of threads available: $(nthreads())"
-#-----------------------------------------------------------------------------
-# load packages
+using Dates
 argtable3 = ArgParseSettings(
     description="This script samples from National.model using NUTS."
 )
@@ -33,7 +16,7 @@ argtable3 = ArgParseSettings(
     "--num-samples", "-n"
         help = "number of samples"
         arg_type = Int
-        default = 1000
+        default = 10
     "--num-chains", "-t"
         help = "number of chains sampled with multi-threading"
         arg_type = Int
@@ -41,7 +24,7 @@ argtable3 = ArgParseSettings(
     "--num-warmup", "-w"
         help = "number of samples to use for warmup/adaptation"
         arg_type = Int
-        default = 1000
+        default = 10
     "--observations-end", "-o"
         help = "end of observation date as string yyyy-mm-dd. Use full data set by defaults"
         arg_type = String
@@ -49,7 +32,11 @@ argtable3 = ArgParseSettings(
     "--cases-start", "-s"
         help = "start date for case observations as string yyyy-mm-dd. No case observations by default"
         arg_type = String
-        default = ""
+        default = "2020-06-01"
+    "--covariates-start", "-p"
+        help = "start date for covariates as string yyyy-mm-dd. starts on 2020-11-10 by default"
+        arg_type = String
+        default = "2020-11-10"
     "--add-covariates", "-c"
         help = "list of covariate names (default empty string = no covariates). Choose: C: contacts, R:"
         arg_type = String
@@ -58,9 +45,6 @@ argtable3 = ArgParseSettings(
         help = "choose from: 'v1', 'gp'. Defaul: 'v1'"
         arg_type = String
         default = "hospit"
-    "--plot-results", "-p"
-        arg_type = Bool
-        default = false
     "--semiparametric", "-S"
         arg_type = Bool
         default = true
@@ -77,52 +61,51 @@ argtable3 = ArgParseSettings(
 end
 parsed_args = parse_args(ARGS, argtable3)
 #-----------------------------------------------------------------------------
-# helper functions for parsing
+# load packages
+@info "load packages"
+using CovidSurvey
+using Random, Turing
+using Base.Threads
+using StatsPlots
+using PrettyTables
+using DataFrames
+using BSON
 
-name2model = Dict(
-    "v3" => National.model_v3,
-    "v2" => National.model_v2,
-    "contacts2" => National.model_contacts_v2,
-    # "contacts" => National.model_contacts,
-    "contacts3" => National.model_contacts_v3,
-    "contacts4" => National.model_contacts_v4,
-    "const-iar" => National.model_constant_iar,
-    "const-iar-c" => National.model_constant_iar_contacts,
-    "contacts-shifted" => National.model_contacts_shifted,
-    "deaths" => National.model_deaths,
-    "cases" => National.model_cases,
-    "hospit" => National.model_hospit,
-    "contacts3" => National.model_contacts_v3,
-    # "parametric-cases" => National.model_parametric_cases,
-    # "parametric-hospit" => National.model_parametric_hospit,
-    # "parametric-deaths" => National.model_parametric_deaths
-)
+using Memoization
+using ReverseDiff
+Turing.setrdcache(true)
+setadbackend(:reversediff)
+
+plotlyjs()
+@info "number of threads available: $(nthreads())"
 #----------------------------------------------------------------------------
 # load data
 @info "load data"
 data = National.load_data(
-      observations_end  = parsed_args["observations-end"]
+      observationsend  = parsed_args["observations-end"]
     , predictors        = parsed_args["add-covariates"]|> CovidSurvey.parse_predictors
-    , rw_step           = 1
-    , epidemic_start    = 30
-    , num_impute        = 6
-    # , deathmodel        = National.DeathInit(obs_stop="$( Date(parsed_args["cases-start"]) + Day(1))")
-    # , casemodel         = National.CaseInit(obs_start=parsed_args["cases-start"])
+    , rwstep           = 1
+    , epidemicstart    = 30
+    , numimpute        = 6
+    , deathmodel        = parsed_args["model"] == "cases" ? National.DeathInit(obs_stop="$( Date(parsed_args["cases-start"]) + Day(30))") : National.DeathInit()
+    , casemodel         = National.CaseInit(obs_start=parsed_args["cases-start"])
     # , hospitmodel       = National.HospitInit()
+    , seromodel         = National.SeroInit1(delay=0, std=.5)
     , link              = KLogistic(3.)
     , invlink           = KLogit(3.)
     , lockdown          = "2020-03-18"
     , covariates_kwargs = Dict(
         :semiparametric => parsed_args["semiparametric"],
-        :fname => projectdir("data","smoothed_contact_rates.csv"),
+        :fname => projectdir("data","raw_contact_rates.csv"),
         :shift => -1,
-        :startdate => "2020-11-10",
+        :startdate => parsed_args["covariates-start"],
         :enddate => nothing))
 
 turing_data = data.turing_data;
 #----------------------------------------------------------------------------
 # prepare model
 ps = (
+    regional = false,
     model    = parsed_args["model"],
     preds    = parsed_args["add-covariates"],
     steps    = parsed_args["num-samples"],
@@ -132,126 +115,29 @@ ps = (
     chains   = parsed_args["num-chains"],
     seed     = parsed_args["seed"],
     semipar  = parsed_args["semiparametric"],
+    predstart= parsed_args["covariates-start"]
 )
 PREFIX = isempty(parsed_args["name-prefix"]) ? "" : parsed_args["name-prefix"]*"-"
 
-const model = name2model[ps.model]
+model = National.name2model[ps.model]
 # model = name2model["hospit"]
 !isnothing(ps.seed) && Random.seed!(ps.seed);
 @info ps
 m = model(turing_data, false)
 #----------------------------------------------------------------------------
-# start sampling
-# function trysample(m, ps)
-#     try
-#         return sample(m, NUTS(ps.warmup, 0.95), ps.steps + ps.warmup; progress=true)
-#     catch e
-#         @error e
-#         return nothing
-#     end
-# end
-#
-# function runsim(m,ps)
-#     for i in 1:100
-#         @info "trial number $i"
-#         chain = trysample(m,ps)
-#         !isnothing(chain) && return chain
-#     end
-#     @error "exceeded number of failed trials"
-# end
-#
-# @time chain = runsim(m,ps)
-@time chain = if ps.chains > 1
-    sample(m, NUTS(ps.warmup, 0.95; max_depth=5), MCMCThreads(), ps.steps + ps.warmup, ps.chains; progress=true)
-else
-    sample(m, NUTS(ps.warmup, 0.95; max_depth=5), ps.steps + ps.warmup; progress=true) #; max_depth=15
-end
-#-----------------------------------------------------------------------------
-# save params
-fdir = projectdir("reports/", parsed_args["foldername"])
-mkpath(fdir)
-@info "Saving at: $fdir"
-safesave( normpath(fdir, PREFIX*"params.csv"), DataFrame( parameter=collect(keys(ps)), value=collect(values(ps)) ) )
-#-----------------------------------------------------------------------------
-@info "plot chain"
-# n = filter( x->!occursin(r"(?<!α)\[", x), String.(names(chain)))
-let
-    n = filter( x->!occursin(r"latent_Rt", x), String.(names(chain)))
-    p = plot(chain[n]);
-    fname = normpath( fdir, savename(PREFIX*"FIG-CHAINSPLOT", ps, "html") )
-    savefig(p, fname )
-    parsed_args["plot-results"] && run(`firefox $(fname)`, wait=false)
-end
-#-----------------------------------------------------------------------------
-@info "meanplot"
-let
-    n = filter( x->!occursin(r"latent_Rt", x), String.(names(chain)))
-    p = meanplot(chain[n]);
-    fname = normpath( fdir, savename(PREFIX*"FIG-MEANPLOT", ps, "html") )
-    savefig(p, fname )
-    parsed_args["plot-results"] && run(`firefox $(fname)`, wait=false)
-end
-#-----------------------------------------------------------------------------
-# save chain
-chain = let chain = chain[ps.warmup+1:10:end,:,:]
-    fname = normpath( fdir, savename(PREFIX*"CHAIN", ps, "jls") )
-    safesave( fname, chain)
-    chain
-end
-#-----------------------------------------------------------------------------
-@info "make predictions"
-generated_posterior = let
-    m_pred = model(turing_data, true)
-    gq = Turing.generated_quantities(m_pred, chain)
-    vectup2tupvec(reshape(gq, length(gq)))
-end
-#-----------------------------------------------------------------------------
-@info "store generated quantities"
-let
-    fname = normpath( fdir, savename(PREFIX*"GENERATED-QUANTITIES", ps, "bson") )
-    dic = Dict( zip( keys(generated_posterior), values(generated_posterior) ) )
-    bson( fname ,  dic )
-end
-#-----------------------------------------------------------------------------
-@info "store reproduction number"
-let
-    rt = let
-        ib = findfirst(==(Date("2020-05-15")), data.dates)
-        de = isnothing(ps.observ) ? last(data.cases.date) : Date(ps.observ)
-        ie = findfirst(==(de), data.dates)
-        Rt_array = hcat(generated_posterior.Rt...)[ib:ie,:]
-
-        qs = [quantile(v, [0.025, 0.25, 0.5, 0.75, 0.975]) for v in eachrow(Rt_array)]
-        llq, lq, mq, uq, uuq = (eachrow(hcat(qs...))..., )
-
-        date = data.dates[ib:ie]
-        DataFrame((;date, llq, lq, mq, uq, uuq))
+@time chain = let
+    thinning = 10
+    if ps.chains > 1
+        sample(m, NUTS(ps.warmup, 0.95; max_depth=5), MCMCThreads(), ps.steps + ps.warmup, ps.chains; progress=true, thinning)
+    else
+        sample(m, NUTS(ps.warmup, 0.95; max_depth=5), ps.steps + ps.warmup; progress=true, thinning) #; max_depth=15
     end
-    fname = normpath( fdir, savename(PREFIX*"Rt", ps, "csv") )
-    save(fname, rt)
 end
 #-----------------------------------------------------------------------------
-@info "plot results"
-let
-    p = National.plot(data, generated_posterior);
-    fname = normpath( fdir, savename(PREFIX*"FIG-PREDICTION", ps, "html") )
-    savefig(p, fname )
-    # savefig(p, projectdir("figures", fname*".png") )
-    parsed_args["plot-results"] && run(`firefox $(fname)`, wait=false)
-end
+@info "save result"
+fdir = projectdir("reports/", parsed_args["foldername"])
+fname = National.save_results(fdir, ps, data, chain)
+
 #-----------------------------------------------------------------------------
-# perform diagnostics
-if ps.chains > 1
-    @info "gelman diagnostics"
-    diagnostics = gelmandiag(chain)
-    fname = normpath( fdir, savename(PREFIX*"GELMANDIAG", ps, "csv") )
-    safesave( fname, diagnostics)
-    pretty_table(diagnostics; crop=:none)
-end
-#-----------------------------------------------------------------------------
-# @info "plot prior vs. posterior"
-# pchain = sample(m, Prior(), ps.steps + ps.warmup; progress=true)[ps.warmup+1:end,:,:]
-# n = filter( x->!occursin(r"\[", x), String.(names(pchain)))
-# p = density(chainscat(pchain[n], chain[n]))
-# savefig(p, projectdir("figures/tmp", fname*"_PRIORvsPOSTERIOR.html") )
-# run(`firefox $(projectdir("figures/tmp", fname*"_PRIORvsPOSTERIOR.html"))`, wait=false)
+@info "post processing"
+National.postprocessing(fname; plot_results=false)
--- scripts/run_regional.jl
+++ scripts/run_regional.jl
@@ -12,6 +12,7 @@ using StatsPlots
 using PrettyTables
 using DataFrames
 using BSON
+using OrderedCollections
 
 using Memoization
 using ReverseDiff
@@ -33,7 +34,11 @@ argtable = ArgParseSettings(
     "--num-samples", "-n"
         help = "number of samples"
         arg_type = Int
-        default = 100
+        default = 10
+    "--rwstep", "-r"
+        help = "random walk step size. Defaults to 7"
+        arg_type = Int
+        default = 7
     "--num-chains", "-t"
         help = "number of chains sampled with multi-threading"
         arg_type = Int
@@ -41,15 +46,15 @@ argtable = ArgParseSettings(
     "--num-warmup", "-w"
         help = "number of samples to use for warmup/adaptation"
         arg_type = Int
-        default = 100
+        default = 10
     "--observations-end", "-o"
         help = "end of observation date as string yyyy-mm-dd. Use full data set by defaults"
         arg_type = String
-        default = nothing
+        default = "2021-02-01"
     "--cases-start", "-s"
-        help = "start date for case observations as string yyyy-mm-dd. No case observations by default"
+        help = "start date for case observations as string yyyy-mm-dd. case observations start on 2020-06-01 by default"
         arg_type = String
-        default = ""
+        default = "2020-06-01"
     "--add-covariates", "-c"
         help = "list of covariate names (default empty string = no covariates). Choose: C: contacts, R:"
         arg_type = String
@@ -58,6 +63,10 @@ argtable = ArgParseSettings(
         help = "choose from: 'hospit', 'cases', 'deaths'. Defaul: 'hospit'"
         arg_type = String
         default = "hospit"
+    "--standardize", "-z"
+        help = "normalize predictors to unit standard deviation"
+        arg_type = Bool
+        default = false
     "--plot-results", "-p"
         arg_type = Bool
         default = false
@@ -67,6 +76,12 @@ argtable = ArgParseSettings(
     "--name-prefix", "-u"
         arg_type = String
         default = ""
+    "--include-early-dynamics", "-i"
+        arg_type = Bool
+        default = true
+    "--semiparametric", "-S"
+        arg_type = Bool
+        default = true
     "--seed"
         help = "random seed to use"
         arg_type = Int
@@ -77,76 +92,82 @@ parsed_args = parse_args(ARGS, argtable)
 #----------------------------------------------------------------------------
 # load data
 @info "load data"
-
 data_params = (
       observationsend   = parsed_args["observations-end"]
-    , predictors        = parsed_args["add-covariates"]|> CovidSurvey.parse_predictors
+    , predictors        = parsed_args["add-covariates"] |> CovidSurvey.parse_predictors
     # , hospitmodel       = Regional.HospitInit1(obs_stop="2020-07-01")
     , casemodel         = Regional.CaseInit1(obs_start=parsed_args["cases-start"])
-    , seromodel         = Regional.SeroInit2(delay=0, std=1.)
-    , rwstep           = 1
-    , epidemicstart    = 10
+    , seromodel         = Regional.SeroInit2(delay=0, std=.5)
+    , rwstep           = parsed_args["rwstep"]
+    , epidemicstart    = 20
     , numimpute        = 6
-    , link              = KLogistic(3.)
-    , invlink           = KLogit(3.)
+    , include_early_dynamic = parsed_args["include-early-dynamics"]
+    , link              = KLogistic(4.5)
+    , invlink           = KLogit(4.5)
     , covariates_kwargs = Dict(
-      :semiparametric => true,
-      :fname => normpath( homedir(), "data/covidsurvey/smoothed_contacts.csv" ),
-      :shift => -1,
-      :startdate => "2020-11-10", # >= 1000 cases / day
+      :fname => [normpath( projectdir("data"), "mean_contact_rates_region=$r.csv" ) for r in 1:Regional.nregions],
+      :mobility => [projectdir("data/google/mobility_region=$(Regional.regions[i]).csv") for i in 1:Regional.nregions],
+      :semiparametric => parsed_args["semiparametric"],
+      :shift          => -1,
+      :startdate      => "2020-11-10", # >= 1000 cases / day
+      :datecol        => "dates",
+      :aggregation    => 1,
+      :standartize    => parsed_args["standardize"],
     )
-)
+    )
 
 data = Regional.load_data(; data_params... )
-
 turing_data = data.turing_data;
 #----------------------------------------------------------------------------
 # prepare model
-ps = Dict( zip( keys(data_params), values(data_params) ) )
-ps[:model]  = parsed_args["model"]
-ps[:steps]  = parsed_args["num-samples"]
-ps[:warmup] = parsed_args["num-warmup"]
-ps[:chains] = parsed_args["num-chains"]
-ps[:seed]   = parsed_args["seed"]
-ps[:folder] = parsed_args["foldername"]
-ps[:prefix] = isempty(parsed_args["name-prefix"]) ? "" : parsed_args["name-prefix"]*"-"
-ps = NamedTuple(ps)
+# ps = OrderedDict( zip( keys(data_params), values(data_params) ) )
+# ps[:regional] = true
+# ps[:model]  = parsed_args["model"]
+# ps[:steps]  = parsed_args["num-samples"]
+# ps[:warmup] = parsed_args["num-warmup"]
+# ps[:chains] = parsed_args["num-chains"]
+# ps[:rwstep] = parsed_args["rwstep"]
+# ps[:seed]   = parsed_args["seed"]
+# ps[:prefix] = isempty(parsed_args["name-prefix"]) ? "" : parsed_args["name-prefix"]*"-"
+# ps = NamedTuple(ps)
+
+ps = (
+        regional = true,
+        model    = parsed_args["model"],
+        preds    = parsed_args["add-covariates"],
+        steps    = parsed_args["num-samples"],
+        warmup   = parsed_args["num-warmup"],
+        observ   = parsed_args["observations-end"],
+        chains   = parsed_args["num-chains"],
+        rwstep   = parsed_args["rwstep"],
+        seed     = parsed_args["seed"],
+        std      = parsed_args["standardize"],
+        semipar  = parsed_args["semiparametric"],
+        prefix   = isempty(parsed_args["name-prefix"]) ? "" : parsed_args["name-prefix"]*"-"
+)
+# cases    = parsed_args["cases-start"],
+# predstart= parsed_args["covariates-start"]
+
 
 const model = Regional.name2model[ps.model]
 !isnothing(ps.seed) && Random.seed!(ps.seed);
 @info ps
-m = model(turing_data, false)
+m = model(turing_data, vectorize=true)
 m();
 ## ==========================================================================
 # sampling
 @time chain = let
     thinning = 10
     if ps.chains > 1
-        sample(m, NUTS(ps.warmup, 0.95; max_depth=5), MCMCThreads(), ps.steps + ps.warmup, ps.chains; progress=true, thinning)
+        sample(m, NUTS(ps.warmup, 0.95; max_depth=7), MCMCThreads(), ps.steps + ps.warmup, ps.chains; progress=true, thinning)
     else
-        sample(m, NUTS(ps.warmup, 0.95; max_depth=5), ps.steps + ps.warmup; progress=true, thinning) #; max_depth=15
+        sample(m, NUTS(ps.warmup, 0.95; max_depth=7), ps.steps + ps.warmup; progress=true, thinning) #; max_depth=15
     end
 end
 ## ==========================================================================
 @info "save"
-fdir = projectdir("reports/", ps.folder)
-ignores = [k for (k,v) in pairs(ps) if ((v isa String) && isempty(v))]
-
-mkpath(fdir)
-@info "Saving at: $fdir"
-fname = let
-    dic = Dict( zip( keys(ps), values(ps) ) )
-    fname = normpath(fdir, savename(ps.prefix*"PARAMS", ps, "csv"; ignores) )
-    safesave( fname, DataFrame( dic ) )
-    bson( normpath(fdir, ps.prefix*"params.bson") ,  dic )
-
-    fname = normpath( fdir, savename(ps.prefix*"DATA", ps, "bson"; ignores) )
-    bson( fname, Dict("data"=>data) )
-
-    fname = normpath( fdir, savename(ps.prefix*"CHAIN", ps, "jls"; ignores) )
-    safesave( fname, chain )
-    fname
-end
+fdir = projectdir("reports", parsed_args["foldername"])
+fname = Regional.save_results(fdir, ps, data, chain)
 
 ## ==========================================================================
 @info "post-processing"
